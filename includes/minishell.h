53d97f7cda2d6855a3a6d077814689570a8ad3b6
na7li <mohamed.nahli@hotmail.com>
2025-06-16 11:59:30 +0100

    organizing things

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mnahli <mnahli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:34:25 by ael-krai          #+#    #+#             */
-/*   Updated: 2025/06/16 11:16:10 by mnahli           ###   ########.fr       */
+/*   Updated: 2025/06/16 11:54:43 by mnahli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,8 @@
 # define NOTFOUND 127
 # define FAILURE 1
 # define SUCCESS 0
-// #define APPEND_ERROR "minishell: syntax error near unexpected token `>>'"
-// #define APPEND_ERROR "minishell: syntax error near unexpected token `<<'"
-// #define APPEND_ERROR "minishell: syntax error near unexpected token `>>'"
-// #define APPEND_ERROR "minishell: syntax error near unexpected token `>>'"
-// #define APPEND_ERROR "minishell: syntax error near unexpected token `>>'"#
 
-// just to silent the error
 int					g_exit_code;
-// end silenting the error
-typedef enum s_type
-{
-	CMD,
-	PIPE,
-	INPUT,
-	OUTPUT,
-	HEREDOC,
-	APPEND
-}					t_type;
 
 typedef struct s_env
 {
@@ -59,7 +43,7 @@ typedef struct s_cmd
 {
 	char			**args;
 	char			**heredoc;
-	char *cmd_path; // 2 stars or one star
+	char 			*cmd_path; // 2 stars or one star
 	pid_t			pid;
 	char			*infile;
 	char			**outfile;
@@ -75,59 +59,58 @@ typedef struct s_fd
 	int				fdout;
 }					t_fd;
 
-// parsing
-
 // ------------------------------ EXECUTION ------------------------------ //
 // env
 int					env_init(t_env **env, char **envp);
 void				increment_shell_lvl(t_env **env);
 
-// exec_main
-void				exec(t_cmd **cmd, t_env **env, char **envp, t_fd *fd);
-int					resolve_cmd_path(char **envp, t_cmd *cmd);
-void				push_env_back(t_env **head, char *value);
-int					ft_heredoc(char *delimiter, t_env *env);
-void				signal_heredoc(int sig);
-int					var_in_line(char *delimiter, char *line);
-void				expand_variables(t_env *env, char *line, int fd);
-char				*get_env_value(t_env *env, const char *key);
-char				*get_var_name(char *var);
-void				setup_signals(void);
-char				*get_next_line(int fd);
-int					open_redir(t_cmd *current, t_fd *fd);
-char				**get_path(char **envp);
+// builtin check
 int					is_builtin(char *command);
-void				close_fds(t_fd *fd);
-int					dup_stdout(t_fd *fd, int newfd);
-int					dup_stdin(t_fd *fd, int newfd);
+int					exec_bltin(t_cmd *current, t_env **env, t_fd *fd);
 void				exec_builtin(char **args, t_env **env);
 
-// built_ins functions
-int					exec_bltin(t_cmd *current, t_env **env, t_fd *fd);
-int					ft_echo(char **args);
-int					ft_cd(char **args, t_env **env);
-int					ft_pwd(void);
-int					print_sorted_env(t_env *env);
+// builtin exit utils
+void				ft_exit(char **args, t_cmd **cmds, t_env **env, t_fd *fd);
 
-char				**env_to_str(t_env *env);
+// env utils
+char				*get_env_value(t_env *env, const char *key);
+char				*get_var_name(char *var);
 char				*get_var_value(char *var);
 int					is_valid_env_var_name(char *var);
-void				append_env_value(t_env *env, char *var);
 
-int					ft_export(char **args, t_env **env);
-int					ft_unset(char **args, t_env **env);
+// redir open
+int					open_redir(t_cmd *current, t_fd *fd);
+int					dup_stdout(t_fd *fd, int newfd);
+int					dup_stdin(t_fd *fd, int newfd);
 
-// Memory Cleanup
+// cmd path utils
+char				**get_path(char **envp);
+int					resolve_cmd_path(char **envp, t_cmd *cmd);
+
+// heredoc
+int					ft_heredoc(char *delimiter, t_env *env);
+
+// cmd helper
+void				exit_func(t_fd *fd, int status);
+void				cmd_files_handler(t_cmd *cmd, t_fd *fd);
+int					check_is_dir(char *cmd);
+int					var_in_line(char *delimiter, char *line);
+
+// signal handler
+void				setup_signals(void);
+void				reset_terminal(void);
+void				signal_heredoc(int sig);
+
+// fds utils
+void				close_fds(t_fd *fd);
+void				init_fds(t_fd *fd);
+
+// expansion
+void				expand_variables(t_env *env, char *line, int fd);
+
+// memory cleanup
 void				free_command_list(t_cmd **cmd_list);
 void				free_array(char **arr);
 void				free_env(t_env **env);
-//**
-
-void				cmd_files_handler(t_cmd *cmd, t_fd *fd);
-void				exit_func(t_fd *fd, int status);
-int					check_is_dir(char *cmd);
-
-void				init_fds(t_fd *fd);
-void				ft_exit(char **args, t_cmd **cmds, t_env **env, t_fd *fd);
 
 #endif
